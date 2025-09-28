a02f7dc97338eb392b32d72460f9c4fa524cb61d
00DemiX00 <alescha.yarkin@yandex.ru>
2025-09-28 21:43:54 +0300

    fix: work on fan charts has been finished

--- vent_basement/src/Components/ui/interactive-chart.tsx
+++ vent_basement/src/Components/ui/interactive-chart.tsx
@@ -7,97 +7,77 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from ".
 export const description = "An interactive area chart"
 
 const chartData = [
-  { date: "2024-04-01", desktop: 81, mobile: 57, fan1: 1, fan2: 0 },
-  { date: "2024-04-02", desktop: 57, mobile: 57, fan1: 1, fan2: 0 },
-  { date: "2024-04-03", desktop: 80, mobile: 80, fan1: 1, fan2: 0  },
-  { date: "2024-04-04", desktop: 242, mobile: 242, fan1: 1, fan2: 0  },
-  { date: "2024-04-05", desktop: 373, mobile: 373, fan1: 1, fan2: 0  },
-  { date: "2024-04-06", desktop: 301, mobile: 301, fan1: 1, fan2: 0  },
-  { date: "2024-04-07", desktop: 245, mobile: 180, fan1: 0, fan2: 0  },
-  { date: "2024-04-08", desktop: 409, mobile: 320, fan1: 0, fan2: 0  },
-  { date: "2024-04-09", desktop: 59, mobile: 110, fan1: 1, fan2: 1  },
-  { date: "2024-04-10", desktop: 261, mobile: 190, fan1: 1, fan2: 1  },
-  { date: "2024-04-11", desktop: 327, mobile: 350, fan1: 1, fan2: 1  },
-  { date: "2024-04-12", desktop: 292, mobile: 210, fan1: 1, fan2: 1  },
-  { date: "2024-04-13", desktop: 342, mobile: 380, fan1: 0, fan2: 1  },
-  { date: "2024-04-14", desktop: 137, mobile: 220, fan1: 0, fan2: 1  },
-  { date: "2024-04-15", desktop: 120, mobile: 170, fan1: 0, fan2: 1  },
-  { date: "2024-04-16", desktop: 138, mobile: 190, fan1: 0, fan2: 1  },
-  { date: "2024-04-17", desktop: 446, mobile: 360, fan1: 0, fan2: 1  },
-  { date: "2024-04-18", desktop: 364, mobile: 410, fan1: 0, fan2: 1 },
-  { date: "2024-04-19", desktop: 243, mobile: 180, fan1: 0, fan2: 1  },
-  { date: "2024-04-20", desktop: 89, mobile: 150, fan1: 0, fan2: 1  },
-  { date: "2024-04-21", desktop: 137, mobile: 200, fan1: 0, fan2: 0  },
-  { date: "2024-04-22", desktop: 224, mobile: 170, fan1: 0, fan2: 0 },
-  { date: "2024-04-23", desktop: 138, mobile: 230, fan1: 0, fan2: 0  },
-  { date: "2024-04-24", desktop: 387, mobile: 290, fan1: 0, fan2: 1  },
-  { date: "2024-04-25", desktop: 215, mobile: 250, fan1: 0, fan2: 1  },
-  { date: "2024-04-26", desktop: 75, mobile: 130, fan1: 0, fan2: 1  },
-  { date: "2024-04-27", desktop: 383, mobile: 420, fan1: 0, fan2: 1  },
-  { date: "2024-04-28", desktop: 122, mobile: 180, fan1: 0, fan2: 1  },
-  { date: "2024-04-29", desktop: 315, mobile: 240, fan1: 0, fan2: 1  },
-  { date: "2024-04-30", desktop: 454, mobile: 380, fan1: 0, fan2: 1  },
-  { date: "2024-05-01", desktop: 165, mobile: 220, fan1: 0, fan2: 1  },
-  { date: "2024-05-02", desktop: 293, mobile: 310, fan1: 0, fan2: 1  },
-  { date: "2024-05-03", desktop: 247, mobile: 190, fan1: 0, fan2: 1  },
-  { date: "2024-05-04", desktop: 385, mobile: 420, fan1: 0, fan2: 1  },
-  { date: "2024-05-05", desktop: 481, mobile: 390, fan1: 0, fan2: 1  },
-  { date: "2024-05-06", desktop: 498, mobile: 520, fan1: 0, fan2: 1  },
-  { date: "2024-05-07", desktop: 388, mobile: 300, fan1: 0, fan2: 1  },
-  { date: "2024-05-08", desktop: 149, mobile: 210, fan1: 0, fan2: 1  },
-  { date: "2024-05-09", desktop: 227, mobile: 180, fan1: 0, fan2: 1  },
-  { date: "2024-05-10", desktop: 293, mobile: 330, fan1: 0, fan2: 1  },
-  { date: "2024-05-11", desktop: 335, mobile: 270, fan1: 0, fan2: 1  },
-  { date: "2024-05-12", desktop: 197, mobile: 240, fan1: 0, fan2: 1  },
-  { date: "2024-05-13", desktop: 197, mobile: 160, fan1: 0, fan2: 1  },
-  { date: "2024-05-14", desktop: 448, mobile: 490, fan1: 0, fan2: 1  },
-  { date: "2024-05-15", desktop: 473, mobile: 380, fan1: 0, fan2: 1  },
-  { date: "2024-05-16", desktop: 338, mobile: 400, fan1: 0, fan2: 1  },
-  { date: "2024-05-17", desktop: 499, mobile: 420, fan1: 0, fan2: 1  },
-  { date: "2024-05-18", desktop: 315, mobile: 350, fan1: 0, fan2: 1  },
-  { date: "2024-05-19", desktop: 235, mobile: 180, fan1: 0, fan2: 1  },
-  { date: "2024-05-20", desktop: 177, mobile: 230, fan1: 0, fan2: 1  },
-  { date: "2024-05-21", desktop: 82, mobile: 140, fan1: 0, fan2: 1  },
-  { date: "2024-05-22", desktop: 81, mobile: 120, fan1: 0, fan2: 1  },
-  { date: "2024-05-23", desktop: 252, mobile: 290, fan1: 0, fan2: 1  },
-  { date: "2024-05-24", desktop: 294, mobile: 220, fan1: 0, fan2: 1  },
-  { date: "2024-05-25", desktop: 201, mobile: 250, fan1: 0, fan2: 1  },
-  { date: "2024-05-26", desktop: 213, mobile: 170, fan1: 0, fan2: 1 },
-  { date: "2024-05-27", desktop: 420, mobile: 460, fan1: 0, fan2: 1  },
-  { date: "2024-05-28", desktop: 233, mobile: 190, fan1: 0, fan2: 1  },
-  { date: "2024-05-29", desktop: 78, mobile: 130, fan1: 0, fan2: 1  },
-  { date: "2024-05-30", desktop: 340, mobile: 280, fan1: 0, fan2: 1  },
-  { date: "2024-05-31", desktop: 178, mobile: 230, fan1: 0, fan2: 1  },
-  { date: "2024-06-01", desktop: 178, mobile: 200, fan1: 0, fan2: 1  },
-  { date: "2024-06-02", desktop: 470, mobile: 410, fan1: 0, fan2: 1  },
-  { date: "2024-06-03", desktop: 103, mobile: 160, fan1: 0, fan2: 1  },
-  { date: "2024-06-04", desktop: 439, mobile: 380, fan1: 0, fan2: 1  },
-  { date: "2024-06-05", desktop: 88, mobile: 140, fan1: 0, fan2: 1  },
-  { date: "2024-06-06", desktop: 294, mobile: 250, fan1: 0, fan2: 1  },
-  { date: "2024-06-07", desktop: 323, mobile: 370, fan1: 0, fan2: 1  },
-  { date: "2024-06-08", desktop: 385, mobile: 320, fan1: 0, fan2: 1  },
-  { date: "2024-06-09", desktop: 438, mobile: 480, fan1: 0, fan2: 1  },
-  { date: "2024-06-10", desktop: 155, mobile: 200, fan1: 0, fan2: 1  },
-  { date: "2024-06-11", desktop: 92, mobile: 150, fan1: 0, fan2: 1  },
-  { date: "2024-06-12", desktop: 492, mobile: 420, fan1: 0, fan2: 1  },
-  { date: "2024-06-13", desktop: 81, mobile: 130, fan1: 0, fan2: 1  },
-  { date: "2024-06-14", desktop: 426, mobile: 380, fan1: 0, fan2: 1  },
-  { date: "2024-06-15", desktop: 307, mobile: 350, fan1: 0, fan2: 1  },
-  { date: "2024-06-16", desktop: 371, mobile: 310, fan1: 0, fan2: 1  },
-  { date: "2024-06-17", desktop: 475, mobile: 520, fan1: 0, fan2: 1  },
-  { date: "2024-06-18", desktop: 107, mobile: 170, fan1: 0, fan2: 1  },
-  { date: "2024-06-19", desktop: 341, mobile: 290, fan1: 0, fan2: 1 },
-  { date: "2024-06-20", desktop: 408, mobile: 450, fan1: 0, fan2: 1  },
-  { date: "2024-06-21", desktop: 169, mobile: 210, fan1: 0, fan2: 1  },
-  { date: "2024-06-22", desktop: 317, mobile: 270, fan1: 0, fan2: 1  },
-  { date: "2024-06-23", desktop: 480, mobile: 530, fan1: 0, fan2: 1  },
-  { date: "2024-06-24", desktop: 132, mobile: 180, fan1: 0, fan2: 1  },
-  { date: "2024-06-25", desktop: 141, mobile: 190, fan1: 0, fan2: 1  },
-  { date: "2024-06-26", desktop: 434, mobile: 380, fan1: 0, fan2: 1  },
-  { date: "2024-06-27", desktop: 448, mobile: 490, fan1: 0, fan2: 1  },
-  { date: "2024-06-28", desktop: 149, mobile: 200, fan1: 0, fan2: 1  },
-  { date: "2024-06-29", desktop: 103, mobile: 160, fan1: 0, fan2: 1  },
-  { date: "2024-06-30", desktop: 446, mobile: 400, fan1: 0, fan2: 1  },
+    { date: "2024-04-21", desktop: 137, mobile: 200, fan1: "off", fan2: "off" },
+  { date: "2024-04-22", desktop: 224, mobile: 170, fan1: "off", fan2: "off" },
+  { date: "2024-04-23", desktop: 138, mobile: 230, fan1: "off", fan2: "off" },
+  { date: "2024-04-24", desktop: 387, mobile: 290, fan1: "off", fan2: "on" },
+  { date: "2024-04-25", desktop: 215, mobile: 250, fan1: "off", fan2: "on" },
+  { date: "2024-04-26", desktop: 75, mobile: 130, fan1: "off", fan2: "on" },
+  { date: "2024-04-27", desktop: 383, mobile: 420, fan1: "off", fan2: "on" },
+  { date: "2024-04-28", desktop: 122, mobile: 180, fan1: "on", fan2: "on" },
+  { date: "2024-04-29", desktop: 315, mobile: 240, fan1: "on", fan2: "on" },
+  { date: "2024-04-30", desktop: 454, mobile: 380, fan1: "on", fan2: "on" },
+  { date: "2024-05-01", desktop: 165, mobile: 220, fan1: "on", fan2: "on" },
+  { date: "2024-05-02", desktop: 293, mobile: 310, fan1: "off", fan2: "on" },
+  { date: "2024-05-03", desktop: 247, mobile: 190, fan1: "off", fan2: "on" },
+  { date: "2024-05-04", desktop: 385, mobile: 420, fan1: "off", fan2: "on" },
+  { date: "2024-05-05", desktop: 481, mobile: 390, fan1: "off", fan2: "on" },
+  { date: "2024-05-06", desktop: 498, mobile: 520, fan1: "off", fan2: "on" },
+  { date: "2024-05-07", desktop: 388, mobile: 300, fan1: "off", fan2: "on" },
+  { date: "2024-05-08", desktop: 149, mobile: 210, fan1: "off", fan2: "on" },
+  { date: "2024-05-09", desktop: 227, mobile: 180, fan1: "off", fan2: "on" },
+  { date: "2024-05-10", desktop: 293, mobile: 330, fan1: "off", fan2: "on" },
+  { date: "2024-05-11", desktop: 335, mobile: 270, fan1: "off", fan2: "on" },
+  { date: "2024-05-12", desktop: 197, mobile: 240, fan1: "off", fan2: "on" },
+  { date: "2024-05-13", desktop: 197, mobile: 160, fan1: "off", fan2: "on" },
+  { date: "2024-05-14", desktop: 448, mobile: 490, fan1: "off", fan2: "on" },
+  { date: "2024-05-15", desktop: 473, mobile: 380, fan1: "off", fan2: "on" },
+  { date: "2024-05-16", desktop: 338, mobile: 400, fan1: "off", fan2: "on" },
+  { date: "2024-05-17", desktop: 499, mobile: 420, fan1: "off", fan2: "on" },
+  { date: "2024-05-18", desktop: 315, mobile: 350, fan1: "off", fan2: "on" },
+  { date: "2024-05-19", desktop: 235, mobile: 180, fan1: "off", fan2: "on" },
+  { date: "2024-05-20", desktop: 177, mobile: 230, fan1: "off", fan2: "on" },
+  { date: "2024-05-21", desktop: 82, mobile: 140, fan1: "off", fan2: "on" },
+  { date: "2024-05-22", desktop: 81, mobile: 120, fan1: "off", fan2: "on" },
+  { date: "2024-05-23", desktop: 252, mobile: 290, fan1: "off", fan2: "on" },
+  { date: "2024-05-24", desktop: 294, mobile: 220, fan1: "off", fan2: "on" },
+  { date: "2024-05-25", desktop: 201, mobile: 250, fan1: "off", fan2: "on" },
+  { date: "2024-05-26", desktop: 213, mobile: 170, fan1: "off", fan2: "on" },
+  { date: "2024-05-27", desktop: 420, mobile: 460, fan1: "off", fan2: "on" },
+  { date: "2024-05-28", desktop: 233, mobile: 190, fan1: "off", fan2: "on" },
+  { date: "2024-05-29", desktop: 78, mobile: 130, fan1: "off", fan2: "on" },
+  { date: "2024-05-30", desktop: 340, mobile: 280, fan1: "off", fan2: "on" },
+  { date: "2024-05-31", desktop: 178, mobile: 230, fan1: "off", fan2: "on" },
+  { date: "2024-06-01", desktop: 178, mobile: 200, fan1: "off", fan2: "on" },
+  { date: "2024-06-02", desktop: 470, mobile: 410, fan1: "off", fan2: "on" },
+  { date: "2024-06-03", desktop: 103, mobile: 160, fan1: "off", fan2: "on" },
+  { date: "2024-06-04", desktop: 439, mobile: 380, fan1: "off", fan2: "on" },
+  { date: "2024-06-05", desktop: 88, mobile: 140, fan1: "off", fan2: "on" },
+  { date: "2024-06-06", desktop: 294, mobile: 250, fan1: "off", fan2: "on" },
+  { date: "2024-06-07", desktop: 323, mobile: 370, fan1: "off", fan2: "on" },
+  { date: "2024-06-08", desktop: 385, mobile: 320, fan1: "off", fan2: "on" },
+  { date: "2024-06-09", desktop: 438, mobile: 480, fan1: "off", fan2: "on" },
+  { date: "2024-06-10", desktop: 155, mobile: 200, fan1: "off", fan2: "on" },
+  { date: "2024-06-11", desktop: 92, mobile: 150, fan1: "off", fan2: "on" },
+  { date: "2024-06-12", desktop: 492, mobile: 420, fan1: "off", fan2: "on" },
+  { date: "2024-06-13", desktop: 81, mobile: 130, fan1: "on", fan2: "off" },
+  { date: "2024-06-14", desktop: 426, mobile: 380, fan1: "on", fan2: "off" },
+  { date: "2024-06-15", desktop: 307, mobile: 350, fan1: "on", fan2: "off" },
+  { date: "2024-06-16", desktop: 371, mobile: 310, fan1: "on", fan2: "off" },
+  { date: "2024-06-17", desktop: 475, mobile: 520, fan1: "on", fan2: "off" },
+  { date: "2024-06-18", desktop: 107, mobile: 170, fan1: "on", fan2: "off" },
+  { date: "2024-06-19", desktop: 341, mobile: 290, fan1: "on", fan2: "off" },
+  { date: "2024-06-20", desktop: 408, mobile: 450, fan1: "on", fan2: "off" },
+  { date: "2024-06-21", desktop: 169, mobile: 210, fan1: "on", fan2: "on" },
+  { date: "2024-06-22", desktop: 317, mobile: 270, fan1: "off", fan2: "on" },
+  { date: "2024-06-23", desktop: 480, mobile: 530, fan1: "off", fan2: "on" },
+  { date: "2024-06-24", desktop: 132, mobile: 180, fan1: "off", fan2: "on" },
+  { date: "2024-06-25", desktop: 141, mobile: 190, fan1: "off", fan2: "on" },
+  { date: "2024-06-26", desktop: 434, mobile: 380, fan1: "off", fan2: "off" },
+  { date: "2024-06-27", desktop: 448, mobile: 490, fan1: "off", fan2: "off" },
+  { date: "2024-06-28", desktop: 149, mobile: 200, fan1: "off", fan2: "off"},
+  { date: "2024-06-29", desktop: 103, mobile: 160, fan1: "off", fan2: "off"},
+  { date: "2024-06-30", desktop: 446, mobile: 400, fan1: "off", fan2: "off"},
 ]
 const chartConfig = {
   visitors: {
@@ -130,13 +110,13 @@ export function ChartAreaInteractive() {
     startDate.setDate(startDate.getDate() - daysToSubtract)
     return date >= startDate
   })
-  const isFan1On = filteredData.some((item) => item.fan1 === 1)
-  const isFan2On = filteredData.some((item) => item.fan2 === 1)
-  const transformedData = filteredData.map((item) => ({
-    ...item,
-    fan1: item.fan1 === 1 ? 1 : null,
-    fan2: item.fan2 === 1 ? 1 : null,
-  }))
+  const isFan1On = filteredData.some((item) => item.fan1 === "on")
+  const isFan2On = filteredData.some((item) => item.fan2 === "on")
+const transformedData = filteredData.map((item) => ({
+  ...item,
+  fan1: item.fan1 === "on" ? 1 : 0,
+  fan2: item.fan2 === "on" ? 1 : 0,
+}))
   return (
     <Card className="aspect-auto w-[950px]">
       <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
@@ -241,27 +221,24 @@ export function ChartAreaInteractive() {
               stroke="var(--color-desktop)"
               stackId="a"
             />
-            {(isFan1On ?? false) && (
-              <Area
+            <Area
               yAxisId="rightFan"
-              type="linear"
+              type="step"
               dataKey="fan1"
-              stroke="#11ff00"
-              fillOpacity={0.3}
-              fill="url(#colorFan1)"
+              stroke="#0008ff96"
+              strokeDasharray="4 2" // пунктирная линия
+              strokeOpacity={0.6}   // чуть прозрачнее
+              fill="rgba(0, 0, 250, 0.15)" // можно сделать без заливки
             />
-            )}
-            {(isFan2On ?? false) && (
-              <Area
+            <Area
               yAxisId="rightFan"
-              type="linear"
+              type="step"
               dataKey="fan2"
-              stroke="#eeff00"
-              fillOpacity={0.3}
-              fill="url(#colorFan2)"
+              stroke="#33ff0071"
+              strokeDasharray="4 2" // пунктирная линия
+              strokeOpacity={0.6}
+              fill="rgba(0, 255, 0, 0.1)"
             />
-            )}
-            
           <ChartLegend content={<ChartLegendContent />} />
           </AreaChart>
         </ChartContainer>
